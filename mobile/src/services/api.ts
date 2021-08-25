b8b4950fc92d4295a3bc4abe8c7a8bf5649169e0
Lucas Fernando de Souza <62787018+LucasSouza17@users.noreply.github.com>
2021-08-25 08:11:14 -0300

    Update api.ts

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://donationdo.com.br/api/",
-  //baseURL: "http://192.168.1.106:3333/"
+  baseURL: "http://192.168.1.106:3333/",
 });
 
 export default api;
